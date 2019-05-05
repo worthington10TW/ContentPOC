73251b315ce917c5fa449b864453f5d44b14f8bf
Matthew Worthington <matthew.worthington@thoughtworks.com>
2019-05-05 14:04:00 +0100

    In memory store to save ids, not children

--- ContentPOC/DAL/InMemoryStore.cs
+++ ContentPOC/DAL/InMemoryStore.cs
@@ -1,5 +1,4 @@
 ﻿using ContentPOC.Model;
-using ContentPOC.Unit.Model;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -10,35 +9,67 @@ namespace ContentPOC.DAL
 {
     public class InMemoryStore : IRepository
     {
-        private readonly ConcurrentDictionary<string, IUnit> _store =
-            new ConcurrentDictionary<string, IUnit>();
+        private readonly ConcurrentDictionary<string, DTO> _store =
+            new ConcurrentDictionary<string, DTO>();
 
         public void Reset() => _store.Clear();
 
         public IUnit Get(string[] domain, Guid id)
         {
             _store.TryGetValue(GenerateId(domain, id), out var value);
-            return value;
+            value?.Unit?.Children?.AddRange(GetChildren(value?.ChildrenIds));
+            return value?.Unit;
         }
 
         public Task<IUnit> SaveAsync(IUnit unit)
         {
             unit.Children.AsParallel().ForAll(u => Save(u));
-            // TODO: this really needs IUnit not to be a DTO.  (We need to map so we don't bleed down.)
             return Task.FromResult(Save(unit));
         }
 
-        private static string GenerateId(string[] domain, Guid id) => $"{string.Join(".", domain)}.{id}";
+        public List<IUnit> GetAll(string[] area)=>
+            _store.Where(x => x.Value?.Unit.Domain.All(d => area.Contains(d)) ?? false)
+              .Select(x => {
+                  var value = x.Value;
+                  value.Unit.Children.AddRange(GetChildren(value.ChildrenIds));
+                  return value.Unit;}).ToList();
 
-        private IUnit Save(IUnit unit) =>
-            _store.AddOrUpdate(
-                GenerateId(unit.Domain, unit.Meta.Id),
-                unit,
-                (key, existingUnit) => existingUnit = unit);
+        private static string GenerateId(string[] domain, Guid id) =>
+            $"{string.Join(".", domain)}.{id}";
 
-        public List<IUnit> GetAll(string[] area) =>
-            _store.Where(x => x.Value?.Domain.All(d => area.Contains(d)) ?? false)
-            .Select(x => x.Value)
-            .ToList();
+        private IUnit Save(IUnit unit)
+        {
+            var dto = new DTO(unit);
+            return _store.AddOrUpdate(
+                 GenerateId(unit.Domain, unit.Meta.Id), dto,
+                 (key, existingUnit) => existingUnit = dto).Unit;
+        }
+
+        private IEnumerable<IUnit> GetChildren(DTO.Child[] children) => children?.Select(x => Get(x.Domain, x.Id));         
     }
+
+    public class DTO
+    {
+        public DTO(IUnit unit)
+        {
+            Unit = unit;
+            ChildrenIds = unit?.Children?.Select(x => new Child(x.Domain, x.Meta.Id))
+                .ToArray();
+        }
+
+        public IUnit Unit { get; private set; }
+        public Child[] ChildrenIds { get; private set; }
+
+        public class Child
+        {
+            public Child(string[] domain, Guid id)
+            {
+                Domain = domain;
+                Id = id;
+            }
+            public string[] Domain { get; set; }
+            public Guid Id { get; set; }
+        }
+    }
+
 }
